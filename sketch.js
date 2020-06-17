eb30e1a02cf20dbf5c92ed5d2c119efcbb010b74
Harrison McEwen <harrisonadmcewen@gmail.com>
2020-06-17 18:13:41 +1000

    Major code refactor

--- sketch.js
+++ sketch.js
@@ -1,136 +1,108 @@
 let img;
-
-// Size of target
-let targetSize = 30;
-// Number of rings
-let targetSamples = 3;
-// Adjust the size of the targets
-let targetScaling = 0.85;
-// larger number == closer point sampled
-// smaller number == more random
-let sampleRange = 4;
-
-let spacing;
-
-let gridSample;
+let ringSize,
+  ringNumber,
+  ringScale,
+  colorSampleRange,
+  ringSpacing,
+  ringInterval;
+let canvasScale = 10;
 
 function preload() {
-  // update path to use your own images
-  img = loadImage("data/lucy-closeup-retouch.png");
+  img = loadImage("data/nan-closeup-2.png");
 }
 
 function setup() {
-  // Canvas is 10 times the size of the image
-  // If you want a different sized image just make the input
-  // image the approprate size
-  createCanvas(img.width * 10, img.height * 10);
-  // print(img.width + " • " + img.height);
+  createCanvas(img.width * canvasScale, img.height * canvasScale);
 
-  // targetSize = floor(random(30, 50));
-  // targetSamples = floor(random(2, 4));
-  // targe dtScaling = random(0.7, 0.98);
-  // sampleRange = floor(random(2, 4));
+  // Initialize random pointerlism parameters
+  ringSize = round(random(20, 50));
+  ringNumber = 3;
+  ringScale = random(0.78, 0.92);
+  colorSampleRange = round(random(3, 8));
+  ringSpacing = ringSize * 1.5;
+  ringInterval = ringSize / canvasScale;
 
-  spacing = targetSize * 1.5;
-  gridSample = targetSize / 10;
+  noLoop();
 }
 
 function draw() {
   background(255);
 
-  let counter = 0;
-
+  // Run through every pixel of portrait image
   let yCount = 0;
-  for (let gridY = 0; gridY < img.height; gridY += gridSample) {
-    let posY = map(gridY, 0, img.height, spacing, height - spacing);
-    if (yCount % 2 == 0) {
-      for (let gridX = 0; gridX < img.width; gridX += gridSample) {
-        let posX = map(gridX, 0, img.width, spacing, width - spacing);
-
-        counter++;
-        // get current color
-        let c = color(img.get(gridX, gridY));
-
-        // greyscale conversion
-        let greyscale = round(
-          red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071
-        );
-
-        // pixel color to fill, greyscale to ellipse size
-        let sizeScale = map(
-          greyscale,
-          255,
-          0,
-          targetSize * 0.7,
-          targetSize * targetScaling,
-          true
-        );
-        //  let sizeScale = targetSize * targetScaling;
-        target(gridX, gridY, posX, posY, sizeScale, targetSamples);
-      }
-    } else {
-      for (let gridX = 0; gridX < img.width - gridSample; gridX += gridSample) {
-        // Adjust x spacing for less image sampling
-        let posX = map(
-          gridX,
+  for (let ringY = 0; ringY < img.height; ringY += ringInterval) {
+    let posY = map(ringY, 0, img.height, ringSpacing, height - ringSpacing);
+    for (let ringX = 0; ringX < img.width; ringX += ringInterval) {
+      let posX;
+      if (yCount % 2 == 0) {
+        posX = map(ringX, 0, img.width, ringSpacing, width - ringSpacing);
+      } else {
+        posX = map(
+          ringX,
           0,
           img.width,
-          spacing * 1.25,
-          width - spacing / 1.25
+          ringSpacing * 1.25,
+          width - ringSpacing / 1.25
         );
-        counter++;
-        // get current color
-        let c = color(img.get(gridX, gridY));
-
-        // greyscale conversion
-        let greyscale = round(
-          red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071
-        );
-
-        // pixel color to fill, greyscale to ellipse size
-        let sizeScale = map(
-          greyscale,
-          255,
-          0,
-          targetSize * 0.7,
-          targetSize * targetScaling,
-          true
-        );
-        // let sizeScale = targetSize * targetScaling;
-
-        target(gridX, gridY, posX, posY, sizeScale, targetSamples);
       }
+
+      // get current color
+      let colorSample = color(img.get(ringX, ringY));
+
+      // brightness conversion
+      let brightness = round(
+        red(colorSample) * 0.222 +
+          green(colorSample) * 0.707 +
+          blue(colorSample) * 0.071
+      );
+
+      // pixel color to fill, brightness to ellipse size
+      let sizeScale = map(
+        brightness,
+        255,
+        0,
+        ringSize * 0.7,
+        ringSize * ringScale,
+        true
+      );
+
+      pointillismPoint(ringX, ringY, posX, posY, sizeScale, ringNumber);
     }
 
     yCount++;
   }
-
-  console.log(counter);
-  noLoop();
 }
 
-function target(gridX, gridY, xPos, yPos, size, circleNum) {
+function pointillismPoint(gridX, gridY, xPos, yPos, size, circleNum) {
   noStroke();
 
-  for (let i = 0; i < circleNum; i++) {
-    let randomSampleX = gridX + random(-size / sampleRange, size / sampleRange);
-    let randomSampleY = gridY + random(-size / sampleRange, size / sampleRange);
+  for (let index = 0; index < circleNum; index++) {
+    let randomSampleX =
+      gridX + random(-size / colorSampleRange, size / colorSampleRange);
+    let randomSampleY =
+      gridY + random(-size / colorSampleRange, size / colorSampleRange);
 
     let samplePoint = color(img.get(randomSampleX, randomSampleY));
 
     // every second loop alternate the color of the ellipse
-
     fill(samplePoint);
     // Calculate the size of the ellipse needed in each loop
-    let currentSize = size - (i * size) / circleNum;
+    let currentSize = size - (index * size) / circleNum;
     ellipse(xPos, yPos, currentSize);
   }
 }
 
-function keyPressed() {
-  saveCanvas("img", "png");
-}
+// function keyPressed() {
+//   saveCanvas(millis(), "png");
+// }
 
 function mousePressed() {
+  ringSize = round(random(20, 50));
+  ringNumber = 3;
+  ringScale = random(0.78, 0.92);
+  colorSampleRange = round(random(3, 8));
+  ringSpacing = ringSize * 1.5;
+  ringInterval = ringSize / canvasScale;
+
   loop();
 }
